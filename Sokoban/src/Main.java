9744ada84dea4d638e0135b5df69a3cf13a637fb
Lorenzo <46032643+LeRoy19@users.noreply.github.com>
2019-05-23 16:20:01 +0200

    borag

--- Sokoban/src/Main.java
+++ Sokoban/src/Main.java
@@ -1,10 +1,6 @@
 import Graphics.*;
 
 import java.awt.BorderLayout;
-import java.awt.GraphicsDevice;
-import java.awt.GraphicsEnvironment;
-import java.awt.event.KeyEvent;
-import java.awt.event.KeyListener;
 import java.io.IOException;
 
 import javax.swing.JFrame;
@@ -15,48 +11,28 @@ public class Main {
 	
 	public static void main(String[] args) throws IOException {
 		
-		GraphicsEnvironment ge = GraphicsEnvironment.getLocalGraphicsEnvironment();
-	    GraphicsDevice gd = ge.getDefaultScreenDevice();
-	    if (gd.isFullScreenSupported()) {
+	
+	    
 		JFrame frame = new JFrame("Sokoban");
 		frame.setUndecorated(true);
 		frame.setResizable(false);
-		gd.setFullScreenWindow(frame);
-		Gui g = new Gui();
-		frame.add(g, BorderLayout.CENTER);
-		g.repaint();
+		frame.setExtendedState(JFrame.MAXIMIZED_BOTH);
 		
+		Gui g = new Gui();
+		GraphicsButton b1 = new GraphicsButton( "pressed - Copia (2).png", "unpressed - Copia.png", 600, 600, 150, 50);
+		g.AddButton(b1);
+		g.setFocusable(true);
+		frame.add(g, BorderLayout.CENTER);
+		frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 		frame.setVisible(true);
-		frame.addKeyListener(new KeyListener() {
-			
-			@Override
-			public void keyTyped(KeyEvent e) {
-				// TODO Auto-generated method stub
-				
-			}
-			
-			@Override
-			public void keyReleased(KeyEvent e) {
-				// TODO Auto-generated method stub
-				
-			}
-			
-			@Override
-			public void keyPressed(KeyEvent e) {
-				// TODO Auto-generated method stub
-				if(e.getKeyCode()==KeyEvent.VK_ESCAPE) {
-					
-				}
-			}
-		});
 	
 	     
 	      
 	      
 		
 		
-		frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-	    }
+		
+	    
 	   
 	}
 		
